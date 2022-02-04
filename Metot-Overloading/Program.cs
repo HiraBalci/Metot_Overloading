0849881f934c0c4111660bad5e4d8773f96a98ba
HiraBalci <93313737+HiraBalci@users.noreply.github.com>
2022-02-04 13:50:20 +0300

    metot-overloading

---
+++ Metot-Overloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+            int outSayi;
+            bool sonuc=int.TryParse(sayi, out  outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2, 3, out int toplam);
+            Console.WriteLine(toplam);
+            // overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            //metot imzası
+            //metotadı+ parametre sayisi + parametre
+            string a = " Hira";
+            string b = " Balcı";
+            instance.EkranaYazdir(a, b);
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int Toplam)
+        {
+            Toplam = a+b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri ,string veri2)
+        {
+            Console.WriteLine(veri+veri2);
+        }
+    }
+}
